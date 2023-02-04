95f67488088c1d19f50692279c4fdc0a105f2c75
Basel Rabia <baselrabia@gmail.com>
2023-02-04 17:10:04 +0200

    Update get the value of int on flip bits.cpp

--- bitmask/get the value of int on flip bits.cpp
+++ bitmask/get the value of int on flip bits.cpp
@@ -7,39 +7,49 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <iostream>
+#include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    
+
      /*
-     
+
      input= 8;
-     3 quesies 
+     3 quesies
      = {0 , 5 , 13} flip
-     
-     
-     
-     
-     
-     */ 
-    
-    
-    
-    int n = 8;
-    int flip[3] = {0,5,13};
-    
+
+     */
+
+
+    int n,flip_n,num;
+
+    cin >> n;
+    cin >> flip_n;
+
+    int flip[flip_n];
+    for (int i = 0; i < flip_n; i++) {
+         cin >> flip[i];
+    }
+
+
     for(int i : flip){
-        n = (i*2) | n;
+        num = std::pow(2, i);
+        n = num | n;
+            //cout<< num << " " <<n <<endl;
+
         // 0000 1000
         // 0000 0001
-        
+
+        // 0000 1001 //9
+
         // 0001 0000
         // cout<< i <<endl;
 
     }
-  
+
      cout<< n <<endl;
 
     return 0;
